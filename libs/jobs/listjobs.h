ce5dd8ffbfb4ebddc5d4a9d10d9d584460464bc6
kjain1810 <kunaljain.2000@yahoo.in>
2020-09-26 23:31:36 +0530

    Fix jobs command and bg is finally working :')

--- libs/jobs/listjobs.h
+++ libs/jobs/listjobs.h
@@ -10,7 +10,50 @@ int listjobs()
     for (int a = 0; a < commandCnt; a++)
     {
         if (getpgid(processesID[a]) >= 0)
-            printf("[%d] Running %s [%d]\n", a + 1, processesName[a], processesID[a]);
+        {
+            // printf("[%d] Running %s [%d]\n", a + 1, processesName[a], processesID[a]);
+            char processFile[1024];
+            strcpy(processFile, "/proc/");
+            char pidinchar[10];
+            int len = 0, tmp = processesID[a];
+            while (tmp > 0)
+            {
+                pidinchar[len++] = 48 + tmp % 10;
+                tmp /= 10;
+            }
+            for (int a = 0; a < len / 2; a++)
+            {
+                char tmp = pidinchar[a];
+                pidinchar[a] = pidinchar[len - a - 1];
+                pidinchar[len - a - 1] = tmp;
+            }
+            pidinchar[len] = '\0';
+            strcat(processFile, pidinchar);
+            strcat(processFile, "/stat");
+            FILE *reader = fopen(processFile, "r");
+            char line[MAX_PROCESS_INFO];
+            fgets(line, MAX_PROCESS_INFO, reader);
+            char ret[MAX_PROCESS_INFO];
+            for (int b = 0; b < MAX_PROCESS_INFO; b++)
+                ret[a] = '\0';
+            len = strlen(line);
+            int cnt = 0, idx;
+            for (idx = 0; idx < len; idx++)
+            {
+                if (line[idx] == ' ')
+                    cnt++;
+                if (cnt == 3 - 1)
+                    break;
+            }
+            idx++;
+            for (int a = 0; line[idx + a] != ' '; a++)
+                ret[a] = line[idx + a];
+            ret[1] = '\0';
+            if (strcmp(ret, "T") == 0)
+                printf("[%d] Stopped %s [%d]\n", a + 1, processesName[a], processesID[a]);
+            else
+                printf("[%d] Running %s [%d]\n", a + 1, processesName[a], processesID[a]);
+        }
         else
             printf("[%d] Stopped %s [%d]\n", a + 1, processesName[a], processesID[a]);
     }
